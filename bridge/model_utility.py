414ce2c6cdf6a93407a887f10e934ba5bd73a624
Sebastian Grobelny <sebastian@plushcare.com>
2019-10-22 17:39:21 -0700

    add time helpers

---
+++ bridge/model_utility.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+from bridge.time_helper import TimeHelper
+
+
+class TimeStampAbstractMixin(models.Model):
+    """
+        This abstract mixin is used to add creation_timestamp and updated_timestamp fields to the model this is added to
+        also it overrides the save method to update the creation_timestamp and updated_timestamp as needed
+    """
+
+    TIMESTAMP_FIELDSETS = ('creation_timestamp', 'updated_timestamp',)
+    # timestamps
+    creation_timestamp = models.DateTimeField(null=True, blank=True)
+    updated_timestamp = models.DateTimeField(null=True, blank=True, db_index=True)
+
+    class Meta:
+        abstract = True
+
+    def update_timestamps(self):
+        # get now as UTC
+        utc_now = TimeHelper.get_uct_now_datetime()
+
+        # if we have never been saved, then set the creation_timestamp to now(UTC)
+        if not self.creation_timestamp:
+            self.creation_timestamp = utc_now
+
+        # always overwrite updated_timestamp to now(UTC) with every save
+        self.updated_timestamp = utc_now
+
+    def save(self, *args, **kwargs):
+        self.update_timestamps()
+        super(TimeStampAbstractMixin, self).save(*args, **kwargs)
---
+++ bridge/time_helper.py
@@ -0,0 +1,33 @@
+import datetime
+
+import pytz
+
+UTC = pytz.utc
+PST = pytz.timezone('America/Los_Angeles')
+
+
+class TimeHelper:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def get_uct_now_datetime(cls):
+        return datetime.datetime.utcnow().replace(tzinfo=UTC)
+
+    @classmethod
+    def get_utc_datetime_from_timestamp(cls, timestamp):
+        return datetime.datetime.utcfromtimestamp(timestamp).replace(tzinfo=UTC)
+
+    @classmethod
+    def convert_utc_datetime_to_pst(cls, utc_datetime_obj):
+        return utc_datetime_obj.astimezone(PST)
+
+    @classmethod
+    def generate_timedelta(cls, days=0, seconds=0, minutes=0, hours=0):
+        timedelta_kwargs = {
+            'days': days,
+            'seconds': seconds,
+            'minutes': minutes,
+            'hours': hours
+        }
+        return datetime.timedelta(**timedelta_kwargs)
