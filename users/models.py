2ea730d2ab8fee7d1bd87da76d5f9d8cbe2384d6
Sebastian Grobelny <sebastian@plushcare.com>
2019-10-24 10:51:28 -0700

    saving content

--- users/models.py
+++ users/models.py
@@ -1,16 +1,42 @@
 from django.contrib.auth.models import (
-    UserManager, BaseUserManager, AbstractBaseUser,
+    BaseUserManager, AbstractBaseUser,
 )
 from django.db import models
 from uuid import uuid4
 
 from bridge.model_utility import TimeStampAbstractMixin
 
+
+class GenericUserManager(BaseUserManager):
+    """Define a model manager for User model with no username field."""
+
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        """Create and save a User with the given email and password."""
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create and save a regular User with the given email and password."""
+        self.is_staff = False
+        self.is_admin = True
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        """Create and save a SuperUser with the given email and password."""
+        self.is_staff = True
+        self.is_admin = True
+        return self._create_user(email, password, **extra_fields)
 class AbstractGenericUser(AbstractBaseUser, TimeStampAbstractMixin):
 
     class Meta:
         abstract = True
-
 
 class GenericUser(AbstractGenericUser):
     email = models.EmailField(
@@ -21,6 +47,9 @@ class GenericUser(AbstractGenericUser):
     middle_name = models.CharField(max_length=40)
     last_name = models.CharField(max_length=40)
     date_of_birth = models.DateTimeField(null=True, blank=True)
+    gender = models.CharField(max_length=2,null=True, blank=True)
+
+
     # used to denote whether this is someone who can acces ORM directly
     is_admin = models.BooleanField(default=False, blank=True)
     # if need to deactivate specific accounts
@@ -35,6 +64,8 @@ class GenericUser(AbstractGenericUser):
                                     models.SET_NULL,
                                     blank=True,
                                     null=True,)
+
+    objects = GenericUserManager()
 
 # TODO::figure out what other info we need to store at the company level
 class Company(TimeStampAbstractMixin):
